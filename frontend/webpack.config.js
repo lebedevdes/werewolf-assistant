04c5f83f4da2ae6b7c9889e8321a7774c33cee81
ai.esin <ai.esin@tensor.ru>
2021-07-13 09:57:35 +0300

    add-eslint-to-webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,18 +1,27 @@
 module.exports = {
   resolve: {
-    extensions: [".ts", ".tsx", ".mjs", ".js", ".json"],
+    extensions: ['.ts', '.tsx', '.mjs', '.js', '.json'],
     alias: {
-      "react-dom": "@hot-loader/react-dom",
-    },
+      'react-dom': '@hot-loader/react-dom'
+    }
   },
   module: {
     rules: [
       {
-        test: /\.tsx$/,
+        test: /\.tsx$|\.ts$/,
         exclude: /node_modules/,
-        use: {
-          loader: "ts-loader"
-        }
+        use: [
+          {
+            loader: 'ts-loader'
+          },
+          {
+            options: {
+              eslintPath: require.resolve('eslint')
+
+            },
+            loader: require.resolve('eslint-loader')
+          }
+        ]
       }
     ]
   }
